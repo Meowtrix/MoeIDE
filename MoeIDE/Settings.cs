a3e582f38330db3bd31a044ea5c1470e1aea8b37
Huo Yaoyuan <huoyaoyuan@hotmail.com>
2016-07-25 20:44:57 +0800

    Settings:Filename

--- MoeIDE/Settings.cs
+++ MoeIDE/Settings.cs
@@ -1,9 +1,14 @@
-﻿using Microsoft.VisualStudio.Shell;
+﻿using System.ComponentModel;
+using System.Drawing.Design;
+using System.Windows.Forms.Design;
+using Meowtrix.MoeIDE.ComponentModel;
+using Microsoft.VisualStudio.Shell;
 
 namespace Meowtrix.MoeIDE
 {
     public sealed class Settings : DialogPage
     {
+        [LocalizedCategory("201"), LocalizedDescription("203"), LocalizedDisplayName("202"), Editor(typeof(FileNameEditor), typeof(UITypeEditor))]
         public string MainBackgroundFilename { get; set; }
     }
 }
