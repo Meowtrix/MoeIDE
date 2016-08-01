00f41895ff4251bc7065acf51983d127179e90ee
Huo Yaoyuan <huoyaoyuan@hotmail.com>
2016-08-01 21:20:36 +0800

    v1.3

--- MoeIDE/MoeIDEPackage.cs
+++ MoeIDE/MoeIDEPackage.cs
@@ -25,7 +25,7 @@ namespace Meowtrix.MoeIDE
     /// </para>
     /// </remarks>
     [PackageRegistration(UseManagedResourcesOnly = true)]
-    [InstalledProductRegistration("#110", "#112", "1.2", IconResourceID = 400)] // Info on this package for Help/About
+    [InstalledProductRegistration("#110", "#112", "1.3", IconResourceID = 400)] // Info on this package for Help/About
     [Guid(PackageGuidString)]
     [ProvideOptionPage(typeof(Settings), nameof(MoeIDE), "General", 0, 0, true)]
     [ProvideAutoLoad(UIContextGuids.NoSolution)]
