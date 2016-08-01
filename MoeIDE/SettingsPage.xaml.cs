5d9a427c46cf3c1108d61fd7a68b704374dd2241
Huo Yaoyuan <huoyaoyuan@hotmail.com>
2016-08-01 16:15:20 +0800

    新SettingsPage

---
+++ MoeIDE/SettingsPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace Meowtrix.MoeIDE
+{
+    /// <summary>
+    /// SettingsPage.xaml 的交互逻辑
+    /// </summary>
+    public partial class SettingsPage : UserControl
+    {
+        public SettingsPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
