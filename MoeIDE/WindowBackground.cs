fd3ae7af6c19c2cafb935e97b28ed48fe90828e4
Huo Yaoyuan <huoyaoyuan@hotmail.com>
2019-12-05 21:21:39 +0800

    The extension can be loaded before main window is created.

--- MoeIDE/WindowBackground.cs
+++ MoeIDE/WindowBackground.cs
@@ -12,6 +12,7 @@ namespace Meowtrix.MoeIDE
     {
         private Border parentBorder;
         private Image imagecontrol;
+        private SettingsModel settings;
         public WindowBackground(Window window)
         {
             if (window.IsLoaded) Window_Loaded(window, null);
@@ -23,20 +24,27 @@ namespace Meowtrix.MoeIDE
         private async void SettingsUpdated(SettingsModel oldSettings, SettingsModel newSettings)
 #pragma warning restore
         {
+            settings = newSettings;
             await ThreadHelper.JoinableTaskFactory.SwitchToMainThreadAsync();
+            if (imagecontrol != null)
+                ApplySettings();
+        }
+
+        private void ApplySettings()
+        {
             try
             {
-                var imagesource = BitmapFrame.Create(new Uri(newSettings.MainBackground.Filename), BitmapCreateOptions.None, BitmapCacheOption.OnLoad);
+                var imagesource = BitmapFrame.Create(new Uri(settings.MainBackground.Filename), BitmapCreateOptions.None, BitmapCacheOption.OnLoad);
                 imagesource.Freeze();
                 imagecontrol.Source = imagesource;
-                imagecontrol.Stretch = newSettings.MainBackground.Stretch;
-                imagecontrol.HorizontalAlignment = newSettings.MainBackground.HorizontalAlignment;
-                imagecontrol.VerticalAlignment = newSettings.MainBackground.VerticalAlignment;
-                var br = new SolidColorBrush(newSettings.MainBackground.BackColor);
+                imagecontrol.Stretch = settings.MainBackground.Stretch;
+                imagecontrol.HorizontalAlignment = settings.MainBackground.HorizontalAlignment;
+                imagecontrol.VerticalAlignment = settings.MainBackground.VerticalAlignment;
+                var br = new SolidColorBrush(settings.MainBackground.BackColor);
                 br.Freeze();
                 parentBorder.Background = br;
-                imagecontrol.Opacity = newSettings.MainBackground.Opacity;
-                double blur = newSettings.MainBackground.Blur;
+                imagecontrol.Opacity = settings.MainBackground.Opacity;
+                double blur = settings.MainBackground.Blur;
                 if (blur == 0.0)
                     imagecontrol.Effect = null;
                 else imagecontrol.Effect = new BlurEffect { Radius = blur };
@@ -62,6 +70,8 @@ namespace Meowtrix.MoeIDE
             Grid.SetRowSpan(parentBorder, 4);
             var rootgrid = (Grid)mainwindow.Template.FindName("RootGrid", mainwindow);
             rootgrid.Children.Insert(0, parentBorder);
+
+            ApplySettings();
         }
     }
 }
